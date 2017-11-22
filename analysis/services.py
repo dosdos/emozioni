6e87ddddc08a83db17caec42c54f9e8d49cbf1de
david <david.santucci@cloudacademy.com>
2017-11-22 16:21:54 +0100

    Get entities and language from analysis

--- analysis/services.py
+++ analysis/services.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 
+from analysis.constants import ENTITY_TYPES
 from analysis.models import Emotion
 from utils.google_natural_language_api import GoogleNLP
 
@@ -18,11 +19,21 @@ def get_nlp_analysis(query):
     return nlp_analysis
 
 
+def get_entities(row_entities):
+    return [{
+        'name': entity.get('name'),
+        'name_type': ENTITY_TYPES.get(entity.get('type')),
+        'wiki_url': entity.get('metadata', {}).get('wikipedia_url'),
+    } for entity in row_entities]
+
+
 def get_sentences(json_dump):
     emotions = {emotion.slug: emotion.category for emotion in Emotion.objects.all()}
     analysis = json.loads(json_dump)
     matching = [emotions.get(token['lemma']) for token in analysis.get('tokens', []) if token['lemma'] in emotions]
     return {
         'sentences': analysis['sentences'] if 'sentences' in analysis else [],
+        'entities': get_entities(analysis.get('entities', [])),
         'emotions': set(matching),
+        'language': analysis.get('language'),
     }
