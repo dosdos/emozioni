aeb898ed74e24affd6be28f698e10e54e5b123d7
david <david.santucci@cloudacademy.com>
2017-11-22 16:54:47 +0100

    Display a sample sentence in the homepage form

--- analysis/views.py
+++ analysis/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
-from analysis.services import get_nlp_analysis, get_sentences
+from .constants import SAMPLE_SENTENCE
 from .forms import SearchForm
+from .services import get_nlp_analysis, get_sentences
 
 
 # from django.contrib.auth.decorators import login_required
 # @login_required(login_url='/admin/login/')
 def home(request, template_name="home.html"):
-    form = SearchForm()
+    form = SearchForm(initial={'query': SAMPLE_SENTENCE})
     analysis = {}
 
     if request.method == 'POST':
