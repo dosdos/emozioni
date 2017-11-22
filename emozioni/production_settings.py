bd5dc615c809215734ec3f08437f367c3123cded
david <david.santucci@cloudacademy.com>
2017-11-22 18:33:09 +0100

    Cosmetics

--- emozioni/production_settings.py
+++ emozioni/production_settings.py
@@ -1,11 +1,12 @@
 import dj_database_url
-from .settings import DATABASES
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Enable Connection Pooling (if desired)
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
